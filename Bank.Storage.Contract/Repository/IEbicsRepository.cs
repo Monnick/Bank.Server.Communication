72fe62b4d77b56f1dc82be696241c496edd5b930
Tim Klug <timklug22@yahoo.de>
2017-04-03 23:20:39 +0200

    add storage handling by a repository

---
+++ Bank.Storage.Contract/Repository/IEbicsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Bank.Communication.Infrastructure.Ebics.Versions.H004;
+
+namespace Bank.Storage.Contract.Repository
+{
+	public interface IEbicsRepository: IRepository<ebicsRequest, Guid>
+	{
+		
+	}
+}
